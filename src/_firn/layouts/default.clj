c1bf13f64a3b8467a653f90342822de3a5a8ff26
holtzermann17 <joseph.corneli@hyperreal.enterprises>
2021-04-16 13:30:46 +0100

    render adjacent and backlinks

--- src/_firn/layouts/default.clj
+++ src/_firn/layouts/default.clj
@@ -1,18 +1,20 @@
 (defn default
-  [{:keys [render partials title build-url]}]
+  [{:keys [render partials] :as config}]
   (let [{:keys [head]} partials]
     [:html
-     (head build-url)
+     (head config)
      [:body
       [:main
        [:article.content
         ;; [:div (render :toc)] ;; Optional; add a table of contents
-        [:h1 title]
-        (render :adjacent-files)
-
+        (when-let [adjacent (render :adjacent-files)] 
+          [:div
+           [:hr]
+           [:div.adjacent
+            adjacent]])
         [:div (render :file)]
         ;; if backlinks exist, store them in a let bindings.
-        (when-let [backlinks (render :backlinks)]
+        (when-let [backlinks (render :backlinks)] 
           [:div
            [:hr]
            [:div.backlinks
